ec3a0d1a4772d71fd1f20e0c6f19e8c5e30ccb84
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2013-01-10 23:58:08 +0100

    First release

---
+++ ProjectTreeTemplater.py
@@ -0,0 +1,193 @@
+import os
+import shutil
+import sublime
+import sublime_plugin
+
+# *** GLOBAL VARS ****
+
+# This will represent the current directory of this application
+currentdir = None
+
+# This is the dictionary that will contain the extension-path key-value pair
+# Ex.:
+#     pdf - /path/to/template.pdf
+#     js - /path/to/template.js
+templateslist = {}
+
+# *** GENERIC FUNCTIONS ****
+
+def noop(fake):
+    pass
+
+def raise_error(msg):
+    sublime.error_message("ProjectTreeTemplater: An error happened. Please look at the statusbar for extended details.")
+    sublime.status_message(msg)
+
+# *** TEMPLATE FUNCTIONS ****
+
+def new_template_file(fullpath):
+    # If the file has no templates
+    if not copy_template(fullpath):
+        # Touch the file
+        open(fullpath,'w').close()
+
+def add_template_to_list(spath):
+    # Remove the - first character
+    if spath[0] == '?':
+        spath = spath.lstrip('?')
+
+    global templateslist
+    tpl = spath.split(':')
+    # Remove the . before the name of the extension (if it is present)
+    tpl[0] = tpl[0].lstrip('.')
+    # Remove the root if inserted
+    tpl[1] = tpl[1].lstrip('/')
+    # Add the extension  to the template list
+    templateslist[tpl[0]] = currentdir + '/' + tpl[1]
+
+def copy_template(spath):
+    bret = False
+
+    # Get path to file, and its extension
+    spath,ext = os.path.splitext(spath)
+    ext = ext.lstrip('.') #Remove the . if present
+
+    # if any template was defined before, let's copy it with the path the user chose
+    if ext in templateslist:
+        try:
+            shutil.copyfile(templateslist[ext],spath + '.' + ext)
+            bret = True
+        except Exception:
+            raise_error("The path of the template for the extension '" + ext + "' does not exists or isn't accessible. Will continue with an empty file.")
+
+    return bret
+
+# *** PARSE FUNCTIONS ****
+
+# Add the path if the path doesn't exists
+def add_path(spath):
+
+    templatepath = None
+    if spath.find(':') > -1:
+        spath,templatepath = spath.split(':')
+
+    # Get the full path to the file we have to add
+    fullpath = currentdir + '/' + spath
+
+    # Remove the / first character, avoid root paths
+    if spath[0] == '/':
+        spath = path.lstrip('/')
+
+    if not os.path.exists(fullpath):
+        # Add the path (if exists)
+        dirs = os.path.dirname(spath)
+        if len(dirs) > 0:
+            fulldirspath = currentdir + '/' + dirs
+            if not os.path.exists(fulldirspath):
+                os.makedirs(fulldirspath)
+
+        if len(os.path.basename(fullpath)) > 0:
+            # If this file has his own template
+            if not templatepath is None:
+                try:
+                    shutil.copyfile(templatepath,fullpath)
+                except Exception:
+                    raise_error("The template for file '" + spath + "' does not exists or you do not have the rights to write in the specified path. Will continue with an empty file or generic template (if defined).")
+                    new_template_file(fullpath)
+            else:
+                new_template_file(fullpath)
+
+# Remove always the path, if it exists
+def remove_path(spath):
+    if spath.find(':') > -1:
+        raise
+
+    # Remove the - first character
+    if spath[0] == '-':
+        spath = spath.lstrip('-')
+
+    # Remove the / first character, avoid root paths
+    if spath[0] == '/':
+        spath = spath.lstrip('/')
+
+    filename = path.basename(spath)
+    fullpath = currentdir + '/' + spath
+    if filename == '*':
+        # Remove all files in that dir
+        clean_dir(path.dirname(fullpath))
+    if path.exists(fullpath):
+        if len(filename) > 0:
+            # Remove the file
+            os.unlink(fullpath)
+
+
+def clean_dir(spath):
+    if os.path.exists(spath):
+        for filename in os.listdir(spath):
+            filepath = spath+'/'+filename
+            if os.path.exists(filepath):
+                os.remove(filepath)
+        os.rmdir(spath)
+
+def update_project_tree(view):
+# get the current dir
+    global currentdir
+    currentdir = '' if view.file_name() is None else view.file_name()
+
+    # This will be the first character in every line which will tell us,
+    # what to do for every line we will parse.
+    # The default function, is 'add_path' if no line begins with any
+    # of this characters.
+    ptcMetaCommands = {
+        '-':remove_path,
+        '#':noop,
+        '?':add_template_to_list
+    }
+
+    if len(currentdir) == 0:
+        # Error
+        raise_error("The file must be saved before using this command and it has to be with a '.stprj' extension.")
+    elif os.path.splitext(os.path.basename(currentdir))[1] == '.stprj':
+        # Get the current directory
+        currentdir = os.path.dirname(currentdir)
+
+        # get non-empty selections
+        regions = [s for s in view.sel() if not s.empty()]
+
+        # if there's no non-empty selection, filter the whole document
+        if len(regions) == 0:
+            regions = [sublime.Region(0, view.size())]
+
+        for region in reversed(regions):
+            lines = view.split_by_newlines(region)
+
+            for line in lines:
+                spath = view.substr(line).strip()
+                if len(spath) > 0:
+                    try:
+                        if spath[0] in ptcMetaCommands:
+                            ptcMetaCommands[spath[0]](spath)
+                        else:
+                            add_path(spath)
+                    except Exception:
+                        raise_error("Syntax Error. Please fix it and try again.")
+                        raise
+        sublime.status_message("All done! Project created :)")
+
+# *** SUBLIME CLASSES ****
+
+class ProjectTreeTemplaterCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        update_project_tree(self.view)
+
+class ProjectTreeTemplaterEventListener(sublime_plugin.EventListener):
+    # Execute the project creation after the user has saved the file
+    def on_post_save(self, view):
+        # Read the global settings for the plugin
+        global_settings = sublime.load_settings(__name__ + '.sublime-settings')
+
+        # Do we need to update the project tree? True if it is not defined
+        should_update = view.settings().get('update_project_on_save', global_settings.get('update_project_on_save', True))
+
+        if should_update:
+            update_project_tree(view)
---
+++ README.md
@@ -0,0 +1,93 @@
+# Project Tree Templater
+### <code>Bring templates to your project!</code>
+
+## Introduction
+
+This is a simple plugin for <code>Sublime Text 2</code> to create a template for your project. But what could a template for a project?
+Imagine that you're creating your project literally from zero. You first have to create a folder called "css" where you will put all your CSS files in. Like before, you'll do a "js" folder, where you'll put your Javascript files in. And finally you'll create also an index.html where you will put your HTML inside of it.
+
+Of course, this could be the probably not-common case of work, but in fact, sometimes happenes, from creating templates to starting little projects.
+
+Today, with this plugin you can automate all your work, with only a few line of meta declarations, and everytime your project file will be saved, it will be reflected to your project folder. Simple as it should be.
+
+## How it works
+
+The only thing you have to do is creating a <code>.stprj</code> file wherever you want, remember than ANYTHING will be created in the same place where, that file, will be saved.
+
+After that, what you have to do is declaring your project structure as likely you're used to do manually.
+For example, the project mentioned in the <code>Introduction</code> before, could be written as the following:
+
+    css/
+    js/
+    index.html
+
+Yes, that's it. Pretty simple and clean! Didn't you remember something? Yeah, Unix path declaration. Nothing more, you have all the basis you would need to use this plugin. Nothing new.
+
+## Reference
+
+* To simply place a comment in your source script use the <code>#</code> character as the first character of your line. An example of use would be:
+
+        # Example of comment
+
+* To simply remove any file from your project everytime you save place the <code>-</code> character as the first character of your line. An example of use would be:
+
+        -css/style.css
+
+    but let we suppose we're going to remove all files inside the folder (and the folder itself), then we have to write:
+
+        -css/*
+
+* To place a *default* template for *every* file which will be created with that extension we will use the <code>?</code> character as the first character of your line. An example of use would be:
+
+        ?css:path/to/tpl.css
+        ...
+        css/style.css
+
+    remember that global templates MUST be declared before ANY path declaration.
+
+* To place a single template for a single file, overriding also the global template (if it exists), we have to write the <code>:</code> character AFTER the path declaration. An example of use would be:
+
+        css/style.css:path/to/tpl.css
+
+### And remember!
+
+If you place a <code>/</code> character at the beginning of any path (template or not), the path will be *always* considered RELATIVE to the project file.
+
+## How to install
+
+Soon it will be available to [Sublime Package Control](http://wbond.net/sublime_packages/package_control).
+So what you have to do is: press code>Ctrl+Shift+P</code>, type <code>install</code>, press <code>Enter</code> then type <code>ProjectTreeTemplater</code> and finally press again the <code>Enter</code> key.
+
+In the meanwhile, to install it you just have to download a copy of this folder, and copy it to the one of your already existing packages. To find it, simply open Sublime Text and then do
+    
+    Preferences -> Browse Packages...
+
+and place it inside that folder.
+
+### Only for Windows!
+
+If you have package control enabled you have to specify it in your list. To do that you simply have to press <code>Ctrl+Shift+P</code> and type <code>Package Control Settings - User</code>, press the <code>Enter</code> key and then, in the list put this inside:
+    
+    "ProjectTreeTemplater"
+
+## License
+
+Project Tree Templater - Bring templates to your project!<br/>
+Copyright (C) 2013 Julian Xhokaxhiu
+
+This program is free software: you can redistribute it and/or modify<br/>
+it under the terms of the GNU General Public License as published by<br/>
+the Free Software Foundation, either version 3 of the License, or<br/>
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,<br/>
+but WITHOUT ANY WARRANTY; without even the implied warranty of<br/>
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br/>
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License<br/>
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## Last but not least
+
+This plugin is far from perfect, so if you find any bug you're free to contribute :)
